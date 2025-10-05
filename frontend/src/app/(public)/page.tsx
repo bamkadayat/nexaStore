1dfe733e083d8a71e6e720429ee15fda6c1cb221
bamkadayat <bamkadayat@gmail.com>
2025-10-05 22:39:51 +0200

    add PopularCategories component to display product categories on the homepage

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,9 +1,14 @@
 import HeroSection from "@/components/landing/HeroSection";
+import PopularCategories from "@/components/landing/PopularCategories";
+
 export default function HomePage() {
   return (
     <div>
       {/* Hero Section */}
       <HeroSection />
+
+      {/* Popular Categories */}
+      <PopularCategories />
     </div>
   )
 }
